b0cc8f5e69d613991e957a82664e3ae6a1f51030
149203 <149203@gmail.com>
2019-01-23 11:27:55 -0800

    use Postman to post to /api/users/register route with a name, email, pw

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
+const gravatar = require('gravatar') // to import user gravatars
+const bcrypt = require('bcryptjs') // to encrypt user passwords
 
 // @route   GET api/users/test
 // @desc    Tests the users route
@@ -12,7 +15,39 @@ router.get('/test', (req, res) => res.json({ msg: 'Users works!' })) // we want 
 // @desc    Registers user
 // @access  Public
 router.post('/register', (req, res) => {
-   msg: 'Users works!'
+   // find One is a mongoose method, body is created with body-parser package and setup in server.js
+   User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+         return res.status(400).json({
+            email: 'Email already exists.',
+         }) // throw an error: user is already registered
+      } else {
+         const { name, email, password } = req.body
+         const avatar = gravatar.url(email, {
+            s: '200', // size: 200px
+            r: 'pg', // rating: PG or below
+            d: 'mm', // default if not found: mm (a blank gravatar)
+         })
+
+         const newUser = new User({
+            name,
+            email,
+            avatar,
+            password,
+         })
+
+         bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+               if (err) throw err
+               newUser.password = hash
+               newUser
+                  .save()
+                  .then(user => res.json(user))
+                  .catch(err => console.log(err))
+            })
+         })
+      }
+   })
 })
 
 module.exports = router
