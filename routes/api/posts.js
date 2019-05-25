8628a2e7fd4b64b1ed675e00f2be8e85dea651dd
149203 <149203@gmail.com>
2019-05-25 15:15:05 -0700

    GET posts and DELETE posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,7 +5,41 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
 const validate_post_input = require('../../validation/post')
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get('/', (req, res) => {
+   const errors = {}
+   Post.find()
+      .sort({ date: 'desc' })
+      .then(posts => {
+         if (posts.length === 0) {
+            errors.noposts = 'There are no posts.'
+            return res.status(400).json(errors)
+         }
+         return res.json(posts)
+      })
+      .catch(err => res.status(404).json(err))
+})
+
+// @route   GET api/posts/:id
+// @desc    Get a post by post ID
+// @access  Public
+router.get('/:id', (req, res) => {
+   const errors = {}
+   Post.findById(req.params.id)
+      .then(post => {
+         if (!post) {
+            errors.nopost = 'There is no post by that ID.'
+            return res.status(400).json(errors)
+         }
+         return res.json(post)
+      })
+      .catch(err => res.status(404).json(err))
+})
 
 // @route   POST api/posts
 // @desc    Create post
@@ -30,6 +64,42 @@ router.post(
          .save()
          .then(post => res.json(post))
          .catch(err => res.status(400).json(err))
+   }
+)
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post by post ID
+// @access  Private
+router.delete(
+   '/:id',
+   passport.authenticate('jwt', { session: false }),
+   (req, res) => {
+      // Only the user that created the post can delete it.
+      Profile.findOne({ user: req.user.id }) // find by the current user
+         .then(profile => {
+            Post.findById(req.params.id) // find the post in question
+               .then(post => {
+                  if (String(post.user) !== req.user.id) {
+                     return res.status(401).json({
+                        // 401 is an unauthorized status
+                        notauthorized:
+                           'User is not authorized to delete this post.',
+                     })
+                  }
+                  post
+                     .delete()
+                     .then(() => {
+                        return res.json(`Post ${req.params.id} is deleted`)
+                     })
+                     .catch(err => res.status(400).json(err))
+               })
+               .catch(err =>
+                  res.status(400).json({ postnotfound: 'Post is not found.' })
+               )
+         })
+         .catch(err =>
+            res.status(400).json({ usernotfound: 'User is not found.' })
+         )
    }
 )
 
